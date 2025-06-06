9b8ffb879d354ba190f379af89315c3d2283f21e
Your Name <you@example.com>
2025-06-06 12:27:23 +0530

    updates

---
+++ routes/User.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  UserLogin,
+  UserRegister,
+  addToCart,
+  getAllCartItems,
+  getAllOrders,
+  placeOrder,
+  removeFromCart,
+} from "../controllers/User.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.post("/signup", UserRegister);
+router.post("/signin", UserLogin);
+
+//cart
+router.get("/cart", verifyToken, getAllCartItems);
+router.post("/cart", verifyToken, addToCart);
+router.patch("/cart", verifyToken, removeFromCart);
+
+//order
+router.get("/order", verifyToken, getAllOrders);
+router.post("/order", verifyToken, placeOrder);
+
+
+
+export default router;
